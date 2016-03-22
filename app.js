60c28b8f4f401d355df434e29749b5fcca855ad7
Swapnil Patil <swapnil.patil@weboniselab.com>
2016-03-22 13:12:34 +0530

    middleware written

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
+var myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,8 +27,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(myLogger);
+
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
