73aa80272e94cf865d2e6a007e4cf6ad905786ab
Swapnil Patil <swapnil.patil@weboniselab.com>
2016-03-22 13:06:15 +0530

    Router middleware

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
